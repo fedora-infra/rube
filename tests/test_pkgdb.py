3da54d38d2369777397767feddeb02dc5ca3105e
RemyD <remyd@civx.us>
2013-03-19 04:31:35 -0400

    Added test for https://admin.stg.fedoraproject.org/pkgdb

---
+++ tests/test_pkgdb.py
@@ -0,0 +1,37 @@
+# This file is part of Rube.
+#
+# Rube is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Rube is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Rube. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     Ralph Bean <rbean@redhat.com>
+#     Remy DeCausemaker <remyd@civx.us>
+
+from selenium import webdriver
+import unittest
+from nose.tools import eq_
+
+from utils import prompt_for_auth
+
+
+class TestPkgDb(unittest.TestCase):
+    def setUp(self):
+        self.auth = prompt_for_auth("FAS")
+        self.driver = webdriver.Firefox()
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_title(self):
+        self.driver.get("https://admin.stg.fedoraproject.org/pkgdb")
+        eq_("Fedora Package Database", self.driver.title)
