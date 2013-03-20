34339873cb36d323e0c49b3675859abac50fa3a9
Ralph Bean <rbean@redhat.com>
2013-03-19 21:52:22 -0400

    Test login to elections.

--- rube/tests/test_elections.py
+++ rube/tests/test_elections.py
@@ -17,17 +17,41 @@
 #     Ralph Bean <rbean@redhat.com>
 #     Remy DeCausemaker <remyd@civx.us>
 
+import time
 import unittest
 from nose.tools import eq_
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.support.ui as ui
 import rube
 from utils import prompt_for_auth
 
 
 class TestElections(unittest.TestCase):
+    timeout = 20000
+    base = "https://admin.stg.fedoraproject.org/voting"
     def setUp(self):
         self.auth = prompt_for_auth("FAS")
         self.driver = rube.get_driver()
 
+    def wait_for(self, target):
+        wait = ui.WebDriverWait(self.driver, self.timeout)
+        wait.until(lambda d: target in d.page_source)
+
     def test_title(self):
-        self.driver.get("https://admin.stg.fedoraproject.org/voting")
+        self.driver.get(self.base)
         eq_("Fedora Elections", self.driver.title)
+
+    def test_login(self):
+        self.driver.get(self.base)
+        eq_("Fedora Elections", self.driver.title)
+        elem = self.driver.find_element_by_css_selector("input.button")
+        elem.click()
+
+        elem = self.driver.find_element_by_css_selector("#user_name")
+        elem.send_keys(self.auth[0])
+        elem = self.driver.find_element_by_css_selector("#password")
+        elem.send_keys(self.auth[1])
+        elem.send_keys(Keys.RETURN)
+
+        time.sleep(2)
+        self.wait_for("Welcome, %s" % self.auth[0])
